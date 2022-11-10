c9aa8df4f28533f205ff33c8bd0e79bc8b33eced
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-11-10 19:31:46 +0200

    Add files via upload

---
+++ ListsAsStacksAndQueues-Lab1/Supermarket.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+people = deque()
+
+
+while True:
+    line = input()
+    if line == 'End':
+        break
+    if line == 'Paid':
+        print('\n'.join(people))
+        people.clear()
+    else:
+        people.append(line)
+
+print(f'{len(people)} people remaining.')
