91eec60dfdbf8e97a82ef84cf06d9a009881c8e4
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-11-13 12:59:08 +0200

    Add files via upload

---
+++ ListsAsStacksAndQueues-Exercise2/FashionBoutique.py
@@ -0,0 +1,30 @@
+clothes = [int(el) for el in input().split(" ")]
+rack_cap = int(input())
+
+
+def print_result(data, result):
+    if data > 0:
+        print(result+1)
+    else:
+        print(result)
+
+
+def check(clothes_data, rack_volume):
+    sum_clothes = 0
+    sum_racks = 0
+
+    while clothes_data:
+        clothe_val = clothes_data.pop()
+        sum_clothes += clothe_val
+        if sum_clothes == rack_volume:
+            sum_clothes = 0
+            sum_racks += 1
+        elif sum_clothes > rack_volume:
+            clothes_data.append(clothe_val)
+            sum_clothes = 0
+            sum_racks += 1
+
+    print_result(sum_clothes, sum_racks)
+
+
+check(clothes, rack_cap)
